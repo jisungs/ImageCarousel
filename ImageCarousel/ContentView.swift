fab7c0cf8cb40740d21cebac4f7230b015c19cee
jisung <jisung.smart@gmail.com>
2020-05-25 12:08:59 +0900

    Initial Commit

---
+++ ImageCarousel/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ImageCarousel
+//
+//  Created by The book Air on 2020/05/25.
+//  Copyright © 2020 The book Air. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
